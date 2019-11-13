6f37bd507e5f6c407c3c011629d8f249f7d57758
Joose Helle <joose.helle@gmail.com>
2019-11-13 20:42:16 +0200

    Add default path & last updated timestamp

--- src/main.py
+++ src/main.py
@@ -8,11 +8,15 @@ mqtt_sub = MqttSubscriber(host='mqtt.hsl.fi', topic='/hfp/v2/journey/ongoing/#')
 sub_monitor = SubMonitor(mqtt_sub, 2)
 mqtt_sub.start_sub()
 
-@app.route("/anomalies")
+@app.route('/')
+def default():
+    return "paths available: /anomalies & /status" 
+
+@app.route('/anomalies')
 def anomaly_logs():
     return jsonify(sub_monitor.get_anomaly_log())
 
-@app.route("/status")
+@app.route('/status')
 def sub_status():
     return jsonify(sub_monitor.get_status())
 
--- src/sub_monitor.py
+++ src/sub_monitor.py
@@ -9,6 +9,7 @@ class SubMonitor:
         self.mqtt_sub = mqtt_sub
         self.monitor_interval_secs = monitor_interval_secs
         self.prev_msg_count = None
+        self.prev_msg_time = None
         self.anomaly_log = []
         self.scheduler = BackgroundScheduler()
         self.scheduler.add_job(self.report_sub_stats, 'interval', seconds=monitor_interval_secs)
@@ -17,9 +18,9 @@ class SubMonitor:
     def report_sub_stats(self):
         current_count = self.mqtt_sub.msg_count
         self.mqtt_sub.reset_msg_count()
+        time_str = datetime.utcnow().strftime('%y/%m/%d %H:%M:%S')
         if (self.prev_msg_count is not None and self.prev_msg_count > 0):
             rate_change_ratio = round((current_count - self.prev_msg_count)/self.prev_msg_count, 2)
-            time_str = datetime.utcnow().strftime('%y/%m/%d %H:%M:%S')
             rate_change_str = str(round(rate_change_ratio * 100))
             msg_rate_str = time_str + ' - ' + str(current_count) +' msg/'+str(self.monitor_interval_secs)+'s - '+ rate_change_str + ' %'
             
@@ -38,6 +39,7 @@ class SubMonitor:
             log(msg_rate_str)
 
         self.prev_msg_count = current_count
+        self.prev_msg_time = time_str
 
     def get_anomaly_log(self):
         return self.anomaly_log
@@ -46,5 +48,6 @@ class SubMonitor:
         return {
             'msg_rate_2s': self.prev_msg_count,
             'broker': self.mqtt_sub.host,
-            'topic': self.mqtt_sub.topic
+            'topic': self.mqtt_sub.topic,
+            'last_msg_time_utc': self.prev_msg_time
         }
