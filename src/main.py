d5a5cdbe75ecac48b54386aff38aac6aa64650eb
Joose Helle <joose.helle@gmail.com>
2020-02-11 14:47:15 +0200

    Add max record size & refactor

--- src/main.py
+++ src/main.py
@@ -10,7 +10,6 @@ from utils.mqtt_recorder import MqttRecorder
 
 log = Logger()
 set_env_vars(log)
-app = Flask(__name__)
 
 mqtt_host = os.getenv('MQTT_HOST')
 mqtt_topic = os.getenv('MQTT_TOPIC')
@@ -18,6 +17,7 @@ msg_rate_monitoring: bool = bool(strtobool(os.getenv('MSG_RATE_MONITORING'))) if
 msg_rate_interval_secs = int(os.getenv('MSG_RATE_INTERVAL_SECS')) if ('MSG_RATE_INTERVAL_SECS' in os.environ) else 5
 recording: bool = bool(strtobool(os.getenv('RECORDING'))) if ('RECORDING' in os.environ) else False
 record_hourly_files: bool = bool(strtobool(os.getenv('RECORD_HOURLY_FILES'))) if ('RECORD_HOURLY_FILES' in os.environ) else False
+max_record_size_gb: float = float(os.getenv('MAX_RECORD_SIZE_GB')) if ('MAX_RECORD_SIZE_GB' in os.environ) else None
 
 mqtt_sub = MqttSubscriber(log, mqtt_host, mqtt_topic)
 sub_monitor = SubMonitor(log, mqtt_sub, msg_rate_interval_secs)
@@ -26,9 +26,12 @@ if (msg_rate_monitoring == True):
     sub_monitor.start()
 
 if (recording == True):
-    mqtt_sub.add_recorder(MqttRecorder(log, 'records/', hourly_files=record_hourly_files))
+    recorder = MqttRecorder(log, 'records/', hourly_files=record_hourly_files, max_record_size_gb=max_record_size_gb)
+    mqtt_sub.add_recorder(recorder)
 
 mqtt_sub.start_sub()
+
+app = Flask(__name__)
 
 @app.route('/')
 def default():
--- src/utils/mqtt_recorder.py
+++ src/utils/mqtt_recorder.py
@@ -1,29 +1,53 @@
+import os
 from apscheduler.schedulers.background import BackgroundScheduler
 from utils.logger import Logger
 from datetime import datetime
 
 class MqttRecorder:
 
-    def __init__(self, log: Logger, records_dir: str = 'records/', hourly_files: bool = False):
+    def __init__(self, log: Logger, records_dir: str = 'records/', hourly_files: bool = False, max_record_size_gb: int = None):
         self.log = log
-        self.records_dir = records_dir
+        self.disabled: bool = False
+        self.records_dir: str = records_dir
         self.current_record_file: str = self.get_new_record_file_name()
-        self.scheduler: None
-        if (hourly_files == True):
-            self.set_recording_to_hourly_files()
-        self.log.info('started recording to: '+ self.current_record_file + ', hourly files: '+ str(hourly_files))
+        self.max_record_size_gb: int = max_record_size_gb
+        self.b_hourly_files: bool = hourly_files
+        self.scheduler = None
+        self.setup_recorder_updater()
+        self.log.info('set maximum record size to: '+ str(max_record_size_gb) +' G')
+        self.log.info('starting mqtt recording to: '+ self.current_record_file)
 
-    def get_new_record_file_name(self):
+    def get_new_record_file_name(self) -> str:
         return self.records_dir + datetime.utcnow().strftime('%y-%m-%d-%H') + 'UTC.txt'
 
-    def record(self, msg):
-        with open(self.current_record_file, 'a') as the_file:
-            the_file.write(msg.topic +' '+ str(msg.payload) + '\n')
+    def record(self, msg) -> None:
+        if (self.disabled != True):
+            with open(self.current_record_file, 'a') as the_file:
+                the_file.write(msg.topic +' '+ str(msg.payload) + '\n')
 
-    def set_recording_to_hourly_files(self):
+    def setup_recorder_updater(self) -> None:
         self.scheduler = BackgroundScheduler()
-        self.scheduler.add_job(self.set_hourly_record_file, 'interval', seconds=1)
 
-    def set_hourly_record_file(self):
+        if (self.b_hourly_files == True):
+            self.log.info('set recording to use hourly files')
+            self.scheduler.add_job(self.maybe_update_record_file, 'interval', seconds=1)
+
+        if (self.max_record_size_gb is not None):
+            self.scheduler.add_job(self.maybe_disable_recorder, 'interval', seconds=1)
+
+        # start scheduler if jobs were added
+        if (self.b_hourly_files == True or self.max_record_size_gb is not None):
+            self.scheduler.start()
+
+    def maybe_update_record_file(self) -> None:
         if (self.current_record_file != self.get_new_record_file_name()):
             self.current_record_file = self.get_new_record_file_name()
+
+    def maybe_disable_recorder(self):
+        records_size = sum(os.path.getsize(self.records_dir + f) for f in os.listdir(self.records_dir) if os.path.isfile(self.records_dir + f))
+        records_size_MB = round(records_size/1000000, 1)
+        records_size_GB = round(records_size_MB/1000, 3)
+        if (self.disabled == False):
+            if (records_size_GB >= self.max_record_size_gb):
+                self.log.warning('reached maximum record size ('+ str(self.max_record_size_gb) +' G), recording disabled from now on')
+                self.disabled = True
